88a26e174faf2360d4007192a58958e617ecc6da
fortunebean <89978842+fortunebean@users.noreply.github.com>
2021-09-20 09:00:56 +0900

    fastlms-1 초기화

---
+++ src/main/java/com/zerobase/fastlms1/Fastlms1Application.java
@@ -0,0 +1,13 @@
+package com.zerobase.fastlms1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fastlms1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fastlms1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/fastlms1/Fastlms1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.fastlms1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fastlms1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
